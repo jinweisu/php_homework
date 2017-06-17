566a9510f5d8e68599e9092e44b8ead8c6b47bc4
jinweisu <aogueisu@gmail.com>
2017-06-17 22:41:59 +0800

    Delete phphw_delete.php

--- 購物車/phphw_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id=$_GET["id"];
-if (isset($_GET["id"])) {
-	while ( list($name, $value) = each($_COOKIE[$id])){
-		setcookie($id."[".$name."]","",time()-3600);
-	}
-}
-header("Location: phphw_shoppingcart.php");
-?>
